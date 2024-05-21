7778fdb2f3dc9b0770ee229be1337b5c73917024
ciyi8189 <ciyi8189@naver.com>
2024-05-21 18:36:04 +0900

    웹페이지 톤 수정

--- app.py
+++ app.py
@@ -924,4 +924,5 @@ def qr():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    # app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run('0.0.0.0',port=5123)
